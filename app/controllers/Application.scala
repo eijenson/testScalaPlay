dfbaba7514d0e8a343f84638591f66bfbc3ed2fc
kob <mako0844420@gmail.com>
2017-12-29 16:51:34 +0900

    Jsonを返すエンドポイントの追加

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,18 +10,16 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.Action
 import play.api.mvc.Controller
 
-class Application @Inject() (taskDao: TaskDao) extends Controller {
+class Application @Inject()(taskDao: TaskDao) extends Controller {
 
-	var accessNum: Integer = 0
+  var accessNum: Integer = 0
 
-	def index = Action {
-		Redirect(routes.TaskController.task)
-	}
+  def index = Action {
+    Redirect(routes.TaskController.task)
+  }
 
-	def randam = Action { implicit request =>
-		accessNum += 1
-		Ok(views.html.randam("回数" + accessNum))
-	}
-
-
+  def randam = Action { implicit request =>
+    accessNum += 1
+    Ok(views.html.randam("回数" + accessNum))
+  }
 }
--- app/controllers/TaskController.scala
+++ app/controllers/TaskController.scala
@@ -18,20 +18,25 @@ import akka.actor.ActorSystem
 import forms.TaskForm
 
 @Singleton
-class TaskController @Inject() (implicit actorSystem: ActorSystem, materializer: Materializer, taskService: TaskService) extends Controller {
-	implicit val inEventFormat = Json.format[TaskForm]
-	implicit val outEventFormat = Json.format[Result]
-	implicit val messageFlowTransformer = MessageFlowTransformer.jsonMessageFlowTransformer[TaskForm, Result]
-	
-	def task = Action.async { implicit request =>
-		taskService.getAllTask().map { case tasks => Ok(views.html.task(tasks)) }
-	}
-	
-	def wsTask = WebSocket.accept[TaskForm, Result] { request =>
-		ActorFlow.actorRef[TaskForm, Result] { out =>
-			TaskActor.props(out, taskService)
-		}
-	}
+class TaskController @Inject()(implicit actorSystem: ActorSystem, materializer: Materializer, taskService: TaskService) extends Controller {
+  implicit val inEventFormat = Json.format[TaskForm]
+  implicit val outEventFormat = Json.format[Result]
+  implicit val messageFlowTransformer = MessageFlowTransformer.jsonMessageFlowTransformer[TaskForm, Result]
+
+  def task = Action.async { implicit request =>
+    taskService.getAllTask().map { case tasks => Ok(views.html.task(tasks)) }
+  }
+
+  def wsTask = WebSocket.accept[TaskForm, Result] { request =>
+    ActorFlow.actorRef[TaskForm, Result] { out =>
+      TaskActor.props(out, taskService)
+    }
+  }
+
+  def taskJson = Action.async { request =>
+    taskService.getAllTask().map { case tasks => Ok(Json.toJson(tasks))
+    }
+  }
 }
 
-case class Result(result:Boolean,mode:String, data: String)
+case class Result(result: Boolean, mode: String, data: String)
